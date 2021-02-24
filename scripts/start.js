03e25428e312fd67bd40bfb714cfc4cf779656dd
Zhenya <zhenyasa@microsoft.com>
2021-02-23 18:41:22 -0800

    typo

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var child = require('child_process');
 var arg = process.argv.slice(2);
-if (!arg && !arg.length) {
+if (!arg || !arg.length) {
   console.error('run command not specified, exiting.');
   process.exit(1);
 }
