ffc1fda405b7f6c921ecf0a52371d3c4ff19e5e4
Zhenya <zhenyasa@microsoft.com>
2021-02-23 18:28:31 -0800

    initial commit

---
+++ scripts/start.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var path = require('path');
+var child = require('child_process');
+var arg = process.argv.slice(2);
+if (!arg && !arg.length) {
+  console.error('run command not specified, exiting.');
+  process.exit(1);
+}
+if (!fs.existsSync(path.resolve(__dirname, '../node_modules'))) {
+  child.execSync('npm install');
+} 
+child.spawn(arg[0], arg.slice(1), { stdio: 'inherit' });
